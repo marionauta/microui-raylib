fe90967497d969d4b7d29f18bf7cd1081d5fa15c
Mario Nachbaur <mario@nachbaur.dev>
2023-08-17 11:31:03 +0200

    feat: handle kayboard modifier keys

--- src/murl.c
+++ src/murl.c
@@ -19,17 +19,58 @@ int murl_text_height(mu_Font font) {
   return rlfont.baseSize;
 }
 
-struct murl_MouseButtonMap {
+struct murl__MouseButtonMap {
   MouseButton rl;
   int mu;
 };
 
-static struct murl_MouseButtonMap murl__mouse_buttons[] = {
+static struct murl__MouseButtonMap murl__mouse_buttons[] = {
     {MOUSE_BUTTON_LEFT, MU_MOUSE_LEFT},
     {MOUSE_BUTTON_RIGHT, MU_MOUSE_RIGHT},
     {MOUSE_BUTTON_MIDDLE, MU_MOUSE_MIDDLE},
     {-1, -1},
 };
+
+static void murl__handle_mouse_buttons(mu_Context *ctx, int x, int y) {
+  for (size_t index = 0;; index++) {
+    struct murl__MouseButtonMap button = murl__mouse_buttons[index];
+    if (button.rl == -1U) {
+      break;
+    }
+    if (IsMouseButtonPressed(button.rl)) {
+      mu_input_mousedown(ctx, x, y, button.mu);
+    } else if (IsMouseButtonReleased(button.rl)) {
+      mu_input_mouseup(ctx, x, y, button.mu);
+    }
+  }
+}
+
+struct murl__KeyboardKeyMap {
+  KeyboardKey rl;
+  int mu;
+};
+
+static struct murl__KeyboardKeyMap murl__keyboard_keys[] = {
+    {KEY_LEFT_SHIFT, MU_KEY_SHIFT},    {KEY_RIGHT_SHIFT, MU_KEY_SHIFT},
+    {KEY_LEFT_CONTROL, MU_KEY_CTRL},   {KEY_RIGHT_CONTROL, MU_KEY_CTRL},
+    {KEY_LEFT_ALT, MU_KEY_ALT},        {KEY_RIGHT_ALT, MU_KEY_ALT},
+    {KEY_ENTER, MU_KEY_RETURN},        {KEY_KP_ENTER, MU_KEY_RETURN},
+    {KEY_BACKSPACE, MU_KEY_BACKSPACE}, {-1, -1},
+};
+
+static void murl__handle_keyboard_keys(mu_Context *ctx) {
+  for (size_t index = 0;; index++) {
+    struct murl__KeyboardKeyMap key = murl__keyboard_keys[index];
+    if (key.rl == -1U) {
+      break;
+    }
+    if (IsKeyPressed(key.rl)) {
+      mu_input_keydown(ctx, key.mu);
+    } else if (IsKeyReleased(key.rl)) {
+      mu_input_keyup(ctx, key.mu);
+    }
+  }
+}
 
 void murl_handle_input(mu_Context *ctx) {
   const int mouse_position_x = GetMouseX();
@@ -37,17 +78,8 @@ void murl_handle_input(mu_Context *ctx) {
   mu_input_mousemove(ctx, mouse_position_x, mouse_position_y);
   const Vector2 mouse_wheel_scroll = GetMouseWheelMoveV();
   mu_input_scroll(ctx, (int)mouse_wheel_scroll.x, (int)mouse_wheel_scroll.y);
-  for (size_t index = 0;; index++) {
-    struct murl_MouseButtonMap button = murl__mouse_buttons[index];
-    if (button.rl == -1U) {
-      break;
-    }
-    if (IsMouseButtonPressed(button.rl)) {
-      mu_input_mousedown(ctx, mouse_position_x, mouse_position_y, button.mu);
-    } else if (IsMouseButtonReleased(button.rl)) {
-      mu_input_mouseup(ctx, mouse_position_x, mouse_position_y, button.mu);
-    }
-  }
+  murl__handle_mouse_buttons(ctx, mouse_position_x, mouse_position_y);
+  murl__handle_keyboard_keys(ctx);
 }
 
 void murl_render_ex(mu_Context *ctx, Color background_color) {
@@ -73,7 +105,10 @@ void murl_render_ex(mu_Context *ctx, Color background_color) {
     } break;
 
     case MU_COMMAND_ICON: {
-      assert(0 && "not implemented");
+      // TODO: handle icons correctly
+      Color icon_color = RL_COLOR_FROM_MU(cmd->icon.color);
+      DrawText("#", cmd->icon.rect.x, cmd->icon.rect.y, cmd->icon.rect.h,
+               icon_color);
     } break;
 
     case MU_COMMAND_CLIP: {
